350bd9b0f6fb8317f6c4a52fe62c48e84a59b740
Radhe gupta <radhe65gupta@hotmail.com>
2022-11-13 15:09:04 +0530

    google_adsense_react_component

---
+++ google_adsense_react_component/src/App.js
@@ -0,0 +1,16 @@
+
+import AdsComponent from './AdsComponent';
+import './App.css';
+
+function App() {
+
+    return (
+        <>
+            <h1>Place To show Google AdSense</h1>
+            <AdsComponent dataAdSlot='X2XXXXXXXX' />
+        </>
+
+    );
+}
+
+export default App;
